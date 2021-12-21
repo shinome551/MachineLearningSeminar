3eb769cebe86228dd26b2631fb56faaabc435fbc
shinome551 <reinshinome@gmail.com>
2021-12-22 02:32:55 +0900

    visualize weight

--- exercise2/mnist.py
+++ exercise2/mnist.py
@@ -2,7 +2,9 @@
 # coding: utf-8
 
 import argparse
+from collections import OrderedDict
 
+import torch
 import torch.nn as nn
 from torchvision.datasets import MNIST, CIFAR10
 from torchvision.transforms import ToTensor, Normalize, Compose
@@ -10,14 +12,23 @@ from torchvision.transforms import ToTensor, Normalize, Compose
 from trainer import Trainer
 
 
+def initSeed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
 def main():
     parser=argparse.ArgumentParser()
-    parser.add_argument('--num_epochs', type=int, default=50)
-    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--num_epochs', type=int, default=10)
+    parser.add_argument('--batch_size', type=int, default=256)
     parser.add_argument('--learning_rate', type=float, default=0.001)
     parser.add_argument('--dataset', default='mnist')
     parser.add_argument('--seed', type=int, default=42)
     args = parser.parse_args()
+
+    initSeed(args.seed)
 
     cfg = {
         'num_epochs': args.num_epochs,
@@ -40,13 +51,13 @@ def main():
     else:
         raise ValueError(f'only mnist or cifar10. {args.dataset} is not implemented.')
 
-    model = nn.Sequential(
-        nn.Flatten(),
-        nn.Linear(784, 10)
-    )
+    model = nn.Sequential(OrderedDict([
+        ('flatten', nn.Flatten()),
+        ('layer1', nn.Linear(784, 10))
+    ]))
 
     print('start training')
-    trainer = Trainer(model, trainset, testset, cfg, args.seed)
+    trainer = Trainer(model, trainset, testset, cfg)
     trainer.run()
 
     
--- exercise2/trainer.py
+++ exercise2/trainer.py
@@ -9,15 +9,11 @@ from torch.utils.data import DataLoader
 
 
 class Trainer:
-    def __init__(self, model, trainset, testset, cfg, seed=42):
-        self.initSeed(seed)
+    def __init__(self, model, trainset, testset, cfg):
         self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
         self.num_epochs = cfg['num_epochs']
-            
         self.model = model.to(self.device)
-
         self.optimizer = Adam(self.model.parameters(), lr=cfg['learning_rate'])
-
         batch_size = cfg['batch_size']
         self.trainloader = DataLoader(trainset, batch_size=batch_size, 
                                 shuffle=True, 
@@ -27,13 +23,7 @@ class Trainer:
                                 shuffle=False,
                                 num_workers=2,
                                 pin_memory=True)
-
-
-    def initSeed(self, seed):
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
+        self.input_shape = trainset[0][0].shape
 
 
     def train(self):
@@ -80,21 +70,33 @@ class Trainer:
         plt.gcf().clear()
 
         x = range(1, len(self.train_hist['trainloss']) + 1)
-        ax1 = self.fig.add_subplot(1, 2, 1)
-        ax1.plot(x, self.train_hist['trainloss'], label='train')
-        ax1.plot(x, self.train_hist['testloss'], label='test')
+        ax1 = self.fig.add_subplot(2, 2, 1)
+        ax1.plot(x, self.train_hist['trainloss'], label='train', marker='o')
+        ax1.plot(x, self.train_hist['testloss'], label='test', marker='o')
         ax1.legend()
         ax1.grid(True)
         ax1.set_xlabel('epoch')
+        ax1.set_xticks(x)
         ax1.set_ylabel('Loss')
 
-        ax2 = self.fig.add_subplot(1, 2, 2)
-        ax2.plot(x, self.train_hist['trainacc'], label='train')
-        ax2.plot(x, self.train_hist['testacc'], label='test')
+        ax2 = self.fig.add_subplot(2, 2, 2)
+        ax2.plot(x, self.train_hist['trainacc'], label='train', marker='o')
+        ax2.plot(x, self.train_hist['testacc'], label='test', marker='o')
         ax2.legend()
         ax2.grid(True)
         ax2.set_xlabel('epoch')
+        ax2.set_xticks(x)
         ax2.set_ylabel('Accuracy')
+
+        weight = self.model.layer1.weight.data[:10]
+        for i in range(10):
+            ax = self.fig.add_subplot(2, 10, 11 + i, xticks=[], yticks=[])
+            if self.model.layer1.__class__.__name__ == 'Linear':
+                img = weight[i].reshape(self.input_shape).mean(0)
+            else:
+                img = weight[i].mean(0)
+            img = (img - img.min()) / (img.max() - img.min())
+            ax.imshow(img, cmap='gray', interpolation='none')
 
         plt.pause(1)
 
@@ -106,6 +108,7 @@ class Trainer:
         self.train_hist['testloss'] = []
         self.train_hist['testacc'] = []
         self.fig = plt.figure(figsize=(9, 6))
+        self.plot()
 
         for epoch in range(self.num_epochs):
             trainloss, trainacc = self.train()
@@ -116,4 +119,6 @@ class Trainer:
             self.train_hist['testloss'].append(testloss)
             self.train_hist['testacc'].append(testacc)
             self.plot()
+
+        plt.show()
 
